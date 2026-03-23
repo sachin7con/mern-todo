22cb118aeff36d3ef5a6c2fbd761e4eea66519b5
sachin7con <sachin7con@gmail.com>
2026-03-23 17:39:02 +0530

    backend ready for deployment

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todo.controller');
-const authController = require('../middleware/authMiddleware');
+const authController = require('../../middleware/authMiddleware');
 
 router.get('/', authController, todoController.getTodos);
 router.post('/', authController, todoController.createTodo);
