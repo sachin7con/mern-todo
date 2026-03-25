a926fd192d0dfc7a9cc614e1db6a02ab0bc703f2
sachin7con <sachin7con@gmail.com>
2026-03-25 15:00:58 +0530

    updated baseURL for production

--- frontend/client/src/api/axios.js
+++ frontend/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "http://localhost:3000/api"
+   baseURL: "https://mern-todo-vv95.onrender.com/api"
 })
 
 API.interceptors.request.use((req) => {
