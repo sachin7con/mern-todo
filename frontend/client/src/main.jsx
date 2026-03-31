aea9b30fa1655d03617e320027dadfa9da3e1fa5
sachin7con <sachin7con@gmail.com>
2026-03-31 16:09:47 +0530

    fix main file AuthContext

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import  AuthProvider  from './context/authContext';
+import  AuthProvider  from './context/AuthContext';
 
 import App from './App.jsx'
 
