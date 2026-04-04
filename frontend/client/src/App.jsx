a86822d9023b92ca3d235be94347c67f3f217599
sachin7con <sachin7con@gmail.com>
2026-04-04 23:00:01 +0530

    fix: Navigate

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-        <Route path="/" element={<useNavigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
